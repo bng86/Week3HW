ac5a42c8ffff767cb88333cd5b3d7f4455a80749
Jimmy <seowrobben@gmail.com>
2016-09-26 18:01:19 +0800

    week03-bmi(03160906)

---
+++ app/src/test/java/org/jimc/jimmy/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package org.jimc.jimmy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        bmi helloWorld=new bmi();
+    }
+
+    @Test
+    public void testName() throws Exception {
+        bmi helloWorld=new bmi();
+
+    }
+}
